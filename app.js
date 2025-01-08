fbea228d34400bfa3176c91f316d17c622b4aea8
Tauhidul Islam Sifat <tauhidulsifat@gmail.com>
2025-01-08 19:01:02 +0600

    add: get post endpoint

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.post("/", (req, res) => {
 app.delete("/", (req, res) => {
   res.send("Hello World deleted");
 });
+app.get("/post:id", (req, res) => {
+  res.send(`Get request params is ${req.params.id} | ${req.path}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
