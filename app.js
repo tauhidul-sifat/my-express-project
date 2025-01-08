0866aab965f09d3a6dbfa34937afdbbe1af65cfc
MD. TARIKUL ISLAM <sifatislam1316@gmail.com>
2025-01-08 16:38:41 +0600

    init folder stcture

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
