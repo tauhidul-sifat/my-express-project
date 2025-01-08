d4bbd6cf67450f3815877646d2fcac7df06e3645
Tauhidul Islam Sifat <tauhidulsifat@gmail.com>
2025-01-08 17:42:22 +0600

    add: post api created

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("Hello World All");
 });
 
+app.post("/", (req, res) => {
+  res.send("Hello World Posted successfully");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
